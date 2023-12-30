c72360b7bdfe150fadd3ed34d52a97a1d667b615
Thomas Hodges <thodges@javascriptonline.org>
2023-12-29 21:43:50 -0800

    with explicit typing

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println(futureValue)
 }
