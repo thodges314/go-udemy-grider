5a81777659500ca8ff4b21596cf4b5363bd1526f
Thomas Hodges <thodges@javascriptonline.org>
2023-12-29 22:31:54 -0800

    future real value

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount, years, expectedReturnRate float64 = 1000, 10, 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
