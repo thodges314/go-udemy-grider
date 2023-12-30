6c31aaef334edb781fe71c14196b56443f05f271
Thomas Hodges <thodges@javascriptonline.org>
2023-12-29 21:38:36 -0800

    starting steps

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+
+	fmt.Println(futureValue)
 }
