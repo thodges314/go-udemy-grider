e486e4597902826a42a556e4959f4d8afb27116a
Thomas Hodges <thodges@javascriptonline.org>
2023-12-29 22:28:42 -0800

    one line

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
-	var years float64 = 10
+	var investmentAmount, years float64 = 1000, 10
+	expectedReturnRate := 5.5
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println(futureValue)
 }
