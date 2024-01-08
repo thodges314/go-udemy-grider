6da6a2e9f2c0b75babf5ab42481c3b96edb953a1
Thomas Hodges <thodges@javascriptonline.org>
2024-01-08 03:34:46 -0800

    Printf

--- investment_calculator.go
+++ investment_calculator.go
@@ -14,15 +14,16 @@ func main() {
 	fmt.Print("Investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Years: ")
-	fmt.Scan(&years)
-
 	fmt.Print("Expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println("Future Value: ", futureValue)
+	fmt.Printf("Future Value: %v\nFuture Value (Adjusted for Inflation): %v\n", futureValue, futureRealValue)
+	// fmt.Println("Future Value (Adjusted for Inflation): ", futureRealValue)
 }
